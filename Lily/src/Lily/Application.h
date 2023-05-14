97360d7b885eccc989f1b1c83610ad51e8215a01
kanand003 <anandkkumar003@gmail.com>
2023-05-14 12:25:14 +0530

    Added Layers and LayerStack.

--- Lily/src/Lily/Application.h
+++ Lily/src/Lily/Application.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "Core.h"
-#include "Events/Event.h"
-#include "Events/ApplicationEvent.h"
+//#include "Events/Event.h"
+//#include "Events/ApplicationEvent.h"
 #include "Window.h"
+#include "Lily/LayerStack.h"
+#include "Lily/Events/Event.h"
+#include "Lily/Events/ApplicationEvent.h"
 
 namespace Lily
 {
@@ -15,11 +18,16 @@ namespace Lily
 		void Run();
 
 		void OnEvent(Event& e);
+
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* layer);
+
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
+		LayerStack m_LayerStack;
 	};
 	//To be defined in Client
 	Application* CreateApplication();
