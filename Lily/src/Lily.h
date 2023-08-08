afdfadeedf6a04fe28a1b8a45299a5d9b7d01736
kanand003 <anandkkumar003@gmail.com>
2023-08-08 22:17:16 +0530

    Added input polling and Lily Key/Mouse codes.

--- Lily/src/Lily.h
+++ Lily/src/Lily.h
@@ -4,6 +4,10 @@
 #include "Lily/Layer.h"
 #include "Lily/Log.h"
 
+#include "Lily/Input.h"
+#include "Lily/KeyCodes.h"
+#include "Lily/MouseButtonCodes.h"
+
 #include "Lily/ImGui/ImguiLayer.h"
 
 //-----------------Entry Point----------------------
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -10,12 +10,21 @@ public:
 
 	void OnUpdate() override
 	{
-		LY_INFO("ExampleLayer::Update");
+		//LY_INFO("ExampleLayer::Update");
+		if (Lily::Input::IsKeyPressed(LY_KEY_TAB))
+			LY_TRACE("Tab key is pressed (poll)!");
 	}
 
 	void OnEvent(Lily::Event& event) override
 	{
-		LY_TRACE("{0}", event);
+		//LY_TRACE("{0}", event);
+		if (event.GetEventType() == Lily::EventType::KeyPressed)
+		{
+			Lily::KeyPressedEvent& e = (Lily::KeyPressedEvent&)event;
+			if (e.GetKeyCode() == LY_KEY_TAB)
+				LY_TRACE("Tab key is pressed (event)!");
+			LY_TRACE("{0}", (char)e.GetKeyCode());
+		}
 	}
 };
 
